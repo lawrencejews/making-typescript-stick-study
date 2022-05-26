641abbd3f887d1762683284641a8fec8cceadf66
lawrencejews <lawrencejews1@gmail.com>
2022-05-26 21:30:40 +0900

    type-challenges

--- challenges/async-communicator/src/index.ts
+++ challenges/async-communicator/src/index.ts
@@ -1,7 +1,13 @@
 import { expectType } from "tsd";
 
 // IMPLEMENT THIS TYPE
-export type WrapForPenpal<T> = any;
+export type WrapForPenpal<T> = {
+  [K in keyof T]: T[K] extends { (...arg: infer Args): infer R }
+  ? R extends Promise<any>
+  ? (...arg: Args) => R // E is already a promise
+  : (...arg: Args) => Promise<R>  // R needs to be wrapped
+  : T[K]; // non methods returns
+};
 
 /**
  * Test Scenario - Do not change anything below this line
@@ -10,12 +16,16 @@ const methods = {
   add(a: number, b: number): number {
     return a + b;
   },
+  addAsyncThing(a: number, b: number): Promise<number> {
+    return Promise.resolve(a + b);
+  },
   subtract(a: number, b: number): number {
     return a - b;
   },
 };
 const asyncMethods: WrapForPenpal<typeof methods> = {} as any;
 
+let addPromise1 = asyncMethods.addAsyncThing(1, 2);
 let addPromise = asyncMethods.add(1, 2);
 expectType<Promise<number>>(addPromise);
 // @ts-expect-error
